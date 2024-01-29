7a74cda2705e505bbe2a2f109ba7ebd4e405f494
ahmadsaif24 <153349616+mtx-ahmad@users.noreply.github.com>
2024-01-29 23:24:52 +0500

    Update BookingForm.js

--- client/src/app/pages/Booking/BookingForm.js
+++ client/src/app/pages/Booking/BookingForm.js
@@ -61,7 +61,7 @@ const BookingForm = () => {
 
     // Validate email
     if (formData.email.trim() === '') {
-      newErrors.email = 'Email field is required';
+      newErrors.email = ' ';
       isValid = false;
     }
     else if(!isValidEmail(formData.email)) {
@@ -71,7 +71,7 @@ const BookingForm = () => {
 
     // Validate phone
     if (formData.phone.trim() === '') {
-        newErrors.phone = 'Phone field is required';
+        newErrors.phone = ' ';
         isValid = false;
     }
     else if(!isValidPhone(formData.phone)) {
@@ -171,10 +171,10 @@ const BookingForm = () => {
                 id="name"
                 value={formData.name}
                 onChange={handleChange}
-                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.name ? 'bg-rose-200' : ''}`}
+                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.name ? 'bg-rose-200' : ''} ${errors.name ? 'border-2 border-red-500' : ''}`}
                 placeholder="Enter your Name"
               />
-              {errors.name && <div className="text-red-500">{errors.name}</div>}
+              {/* {errors.name && <div className="text-red-500">{errors.name}</div>} */}
             </div>
             <div>
               <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
@@ -184,7 +184,7 @@ const BookingForm = () => {
                 id="email"
                 value={formData.email}
                 onChange={handleChange}
-                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.email ? 'bg-rose-200' : ''}`}
+                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.email ? 'bg-rose-200' : ''} ${errors.email ? 'border-2 border-red-500' : ''}`}
                 placeholder="Enter your Email"
               />
               {errors.email && <div className="text-red-500">{errors.email}</div>}
@@ -197,7 +197,7 @@ const BookingForm = () => {
                 id="phone"
                 value={formData.phone}
                 onChange={handleChange}
-                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.phone ? 'bg-rose-200' : ''}`}
+                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.phone ? 'bg-rose-200' : ''} ${errors.phone ? 'border-2 border-red-500' : ''}`}
                 placeholder="Enter your Phone Number"
               />
               {errors.phone && <div className="text-red-500">{errors.phone}</div>}
@@ -209,10 +209,10 @@ const BookingForm = () => {
                 name="date"
                 value={formData.date}
                 onChange={handleChange}
-                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.date ? 'bg-rose-200' : ''}`}
+                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.date ? 'bg-rose-200' : ''} ${errors.date ? 'border-2 border-red-500' : ''}`}
                 placeholder="Enter Date"
               />
-              {errors.date && <div className="text-red-500">{errors.date}</div>}
+              {/* {errors.date && <div className="text-red-500">{errors.date}</div>} */}
             </div>
             <div>
               <label htmlFor="startTime" className="block text-sm font-medium text-gray-700">Start Time</label>
@@ -221,10 +221,10 @@ const BookingForm = () => {
                 name="startTime"
                 value={formData.startTime}
                 onChange={handleChange}
-                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.startTime ? 'bg-rose-200' : ''}`}
+                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.startTime ? 'bg-rose-200' : ''} ${errors.startTime ? 'border-2 border-red-500' : ''}`}
                 placeholder="Enter Time"
               />
-              {errors.startTime && <div className="text-red-500">{errors.startTime}</div>}
+              {/* {errors.startTime && <div className="text-red-500">{errors.startTime}</div>} */}
             </div>
             <div>
               <label htmlFor="endTime" className="block text-sm font-medium text-gray-700">End Time</label>
@@ -233,22 +233,22 @@ const BookingForm = () => {
                 name="endTime"
                 value={formData.endTime}
                 onChange={handleChange}
-                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.endTime ? 'bg-rose-200' : ''}`}
+                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.endTime ? 'bg-rose-200' : ''} ${errors.endTime ? 'border-2 border-red-500' : ''}`}
                 placeholder="Enter Time"
               />
-              {errors.endTime && <div className="text-red-500">{errors.endTime}</div>}
+              {/* {errors.endTime && <div className="text-red-500">{errors.endTime}</div>} */}
             </div>
-            <div>
+            <div className="col-span-2">
               <label htmlFor="addInfo" className="block text-sm font-medium text-gray-700">Additional Information</label>
               <textarea
-                rows={3}
+                rows={4}
                 name="addInfo"
                 value={formData.addInfo}
                 onChange={handleChange}
-                className={`mt-1 p-2 border border-black-on-active border-3 w-full ${errors.addInfo ? 'bg-rose-200' : ''}`}
+                className={`mt-1 p-2 border border-black-on-active border-3 w-full block p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border-gray-300   ${errors.addInfo ? 'bg-rose-200' : ''}`}
                 placeholder="Enter Additional Information"
               ></textarea>
-              {errors.addInfo && <div className="text-red-500">{errors.addInfo}</div>}
+              {/* {errors.addInfo && <div className="text-red-500">{errors.addInfo}</div>} */}
             </div>
           </div>
           <div className="col-12 mt-5 space-x-2">
